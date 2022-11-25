0b8eedb1183a61eca5408ddbb09d9f9d4708ecf3
Alexei <67167998+alexeiweb@users.noreply.github.com>
2022-11-25 16:15:43 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs =  [
+  {
+    name: 'Beathrice',
+    path: 'audio/Beathrice.mp3',
+    artist: 'Rocking Bocking',
+    cover: 'img/rockin-bockin.jpg'
+  },
+  {
+    name: 'Jungle',
+    path: 'audio/Jungle.mp3',
+    artist: 'Cover',
+    cover: 'img/covers.jpg'
+  },
+  {
+    name: 'Autumn Rain',
+    path: 'musicbox/audio/Autumn Rain.mp3',
+    artist: 'Melancholic',
+    cover: 'img/melancholic.jpg'
+  },
+]
